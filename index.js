b4e4d99d3c8839ec92cc7037f3955deef7ff7485
Achi Onyinye <achionyinyegiftbarbie@gmail.com>
2021-10-18 14:54:28 +0100

    other jobs done

---
+++ index.js
@@ -0,0 +1,48 @@
+
+// let addBtn = document.querySelector("#buttonIncrease");
+// let subBtn = document.querySelector("#buttonDecrease");
+// let qty = document.querySelector("#qtyBox");
+// addBtn.addEventListener('click'   ,()=>{
+//     qty.value = parseInt(qty.value) + 1;
+// });
+// subBtn.addEventListener('click', ()=>{
+//     if (qty.value <= 0){
+//         qty.value = 0;
+//     }else{
+//      qty.value = parseInt (qty.value) - 1;
+//     }
+    
+// });
+
+
+
+
+
+// let decreaseBtn = document.getElementById("buttonDecrease");
+// let increaseBtn = document.getElementById("buttonIncrease");
+// let counter = document.getElementById("countNum");
+// let count= 0;
+// decreaseBtn.addEventListener('click', ()=>{
+//     count--;
+//     counter.innerHTML = counter;
+// })
+
+
+
+// function imcrementButton() {
+// let element = document.getElementById("incrementText");
+// let value = element.innerHTML;
+// ++value;
+
+// console.log(value)
+
+// document.getElementById("incrementText").innerHTML = value;
+// }
+
+// function increment() {
+//       document.getElementById('demoInput').stepUp();
+//    }
+//    function decrement() {
+//       document.getElementById('demoInput').stepDown();
+//    }
+
